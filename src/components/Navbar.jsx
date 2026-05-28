a2691105e138681aa99f50c99c32b1caad0b8b46
jeeva <jeeva@jeevas-MacBook-Air.local>
2026-05-28 09:41:28 +0530

    fixed github pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,8 +45,7 @@ export default function Navbar() {
             alt="Quantum Computing Lab Logo"
           />
           <span className="ml-3 text-white uppercase font-black hidden sm:block text-xs tracking-wide leading-tight">
-            CENTER FOR<br />
-            <span className="text-amber-500">QUANTUM COMMUNICATION AND COMPUTING</span>
+            C<span className="text-amber-500">QCC</span>
           </span>
         </NavLink>
 
