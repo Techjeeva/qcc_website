4bd137fc9968043eda8ed6564b2de3e203d620e9
jeeva <jeeva@jeevas-MacBook-Air.local>
2026-06-28 10:12:03 +0530

    fixed github pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base: '/qcc_website', // Ensures correct asset paths for GitHub Pages deployment
+  //base: '/qcc_website', // Ensures correct asset paths for GitHub Pages deployment
+  base: process.env.VITE_BASE_PATH || '/qcc_website', // Use environment variable for base URL, default to '/'
 })
